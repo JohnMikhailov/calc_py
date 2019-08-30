0ef23d2346a6e73faa68f246618d907d0720a7de
JohnMikhailov <34044587+JohnMikhailov@users.noreply.github.com>
2019-08-30 15:41:58 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,23 +1,23 @@
 from functools import reduce
 
 
-def add(args):
+def add(*args):
     return sum(args)
 
 
-def sub(args):
+def sub(*args):
     return reduce(lambda x, y: x - y, args)
 
 
-def mult(args):
+def mult(*args):
     return reduce(lambda x, y: x * y, args)
 
 
-def div(args):
+def div(*args):
     return reduce(lambda x, y: x / y, args)
 
 
-def power(args):
+def power(*args):
     return reduce(lambda x, y: x ** y, args)
 
 
